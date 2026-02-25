886952d82a85d98bf04e0eeab963f0674b753f3b
Noah Gregory <noah@wts.dev>
2026-02-25 12:13:28 -0500

    minor comment wording change

--- macos/Sources/Helpers/ObjCExceptionCatcher.h
+++ macos/Sources/Helpers/ObjCExceptionCatcher.h
@@ -1,7 +1,7 @@
 #import <Foundation/Foundation.h>
 
 /// This file contains wrappers around various ObjC functions so we can catch
-/// exceptions, since you can't natively catch ObjC exceptions from Swift
+/// exceptions, since you can't safely have ObjC exceptions cross into Swift
 /// (at least at the time of writing this comment).
 
 /// NSWindow.addTabbedWindow wrapper
